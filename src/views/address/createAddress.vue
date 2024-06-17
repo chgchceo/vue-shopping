fe6e879cfa4ad4c545e6529799aec33002db5ef8
chgchceo <463219366@qq.com>
2024-06-17 22:13:15 +0800

    wanshan

--- src/views/address/createAddress.vue
+++ src/views/address/createAddress.vue
@@ -1,21 +1,28 @@
 <template>
+  <div>
+    <van-nav-bar
+      @click-left="$router.go(-1)"
+      left-arrow
+      title="新增地址"
+    ></van-nav-bar>
+    <div class="body">
+      <div class="name">姓名 <input type="text" name="" id="" /></div>
 
-    <div>
-<div>新增地址</div>
+      <div class="region">城市</div>
 
-<button @click="doneButt">确定</button>
-
+      <div>详细地址</div>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
     </div>
 
+    <button class="done" @click="doneButt">确定</button>
+  </div>
 </template>
 
 <script>
 import { addAddressFn } from '@/api/address'
 
 export default {
-
   methods: {
-
     async doneButt () {
       await addAddressFn()
       this.$toast.success('success')
@@ -26,4 +33,24 @@ export default {
 
 <style>
 
+.body{
+
+    padding: 20px;
+
+}
+.region{
+
+    margin: 15px 0;
+}
+.done {
+  position: fixed;
+  width: 90vw;
+  height: 45px;
+  margin-left: 5vw;
+  background-color: purple;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  bottom: 5vw;
+}
 </style>
--- src/views/address/index.vue
+++ src/views/address/index.vue
@@ -1,14 +1,103 @@
 <template>
   <div>
-    <div>地址列表</div>
+    <!-- <div>地址列表</div>地址列表 -->
+    <van-nav-bar   @click-left="onClickLeft" left-arrow title="地址列表"/>
 
-    <button @click="$router.push('/addAddress')">新增地址</button>
+    <div class="item" v-for="item in list" :key="item.address_id">
+
+        <div class="title"><span>{{ item.name }}  {{  item.phone}}</span></div>
+
+        <div  class="address">
+            <div class="con">
+<span>{{ item.region.province+item.region.city+item.region.region + item.detail }}</span>
+
+            </div>
+
+            <button class="edit">编辑</button>
+        </div>
+
+        <div class="line"></div>
+    </div>
+    <button class="btn" @click="$router.push('/addAddress')">新增地址</button>
   </div>
 </template>
 
 <script>
-export default {}
+import { getAddressList } from '@/api/address'
+
+export default {
+
+  data () {
+    return {
+
+      list: []
+
+    }
+  },
+  computed: {
+
+  },
+  async created () {
+    const res = await getAddressList()
+    console.log(res)
+
+    this.list = res.data.list
+  },
+  methods: {
+
+    onClickLeft () {
+      this.$router.go(-1)
+    }
+  }
+}
 </script>
 
 <style>
+
+.item{
+
+    /* padding: 15px; */
+    margin-top: 15px;
+    color: gray;
+}
+
+.line{
+
+    height: 1px;
+    background-color: #e5e5e5;
+}
+.title{
+
+    margin:0 15px;
+}
+.address{
+
+    margin: 15px;
+    display: flex;
+    justify-content: space-between;
+}
+.con{
+
+width:60vw;
+}
+.edit{
+
+    width: 50px;
+    height: 30px;
+}
+.btn{
+
+    position: fixed;
+
+    bottom: 5vw;
+    height: 45px;
+    width: 90vw;
+    margin-left: 5vw;
+    border: none;
+    background-color: purple;
+    border-radius: 20px;
+    color: white;
+
+}
+
 </style>
